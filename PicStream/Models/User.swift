42195e7f0c1c9eaad54897ecd20d8ee84de75856
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-02-26 20:10:22 +0400

    add user model

---
+++ PicStream/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  PicStream
+//
+//  Created by Saba Gogrichiani on 26.02.24.
+//
+
+import Foundation
+
+struct User: Equatable {
+  let id: String
+  let email: String
+  let password: String
+  let age: Int
+}
+
