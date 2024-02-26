9487406ba0f70586129d2760d341de2e95f563a2
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-02-26 19:09:54 +0400

    initial commit

---
+++ PicStream/PicStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicStreamApp.swift
+//  PicStream
+//
+//  Created by Saba Gogrichiani on 26.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PicStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AuthorizationView()
+        }
+    }
+}
