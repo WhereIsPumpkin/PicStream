500de71b6b8886d41c4193978e4228db0db23357
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-02-28 16:35:45 +0400

    Refactor ImageCollectionView for clarity and fix dataSource/delegate assignment

---
+++ PicStream/Scenes/Main/View/Cell/ImageCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ImageCollectionViewCell.swift
+//  PicStream
+//
+//  Created by Saba Gogrichiani on 28.02.24.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "ImageCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("I'm initialized")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PicStream/Scenes/Main/View/UIKitWrapper/ImageCollectionView.swift
+++ PicStream/Scenes/Main/View/UIKitWrapper/ImageCollectionView.swift
@@ -9,11 +9,58 @@ import SwiftUI
 import UIKit
 
 struct ImageCollectionView: UIViewRepresentable {
+    @Binding var images: [ImageModel]
+    @Binding var selectedImage: ImageModel?
+    
     func makeUIView(context: Context) -> UICollectionView {
-        
+        return createCollectionView(context: context)
     }
     
     func updateUIView(_ uiView: UICollectionView, context: Context) {
+        uiView.reloadData()
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UICollectionViewDataSource, UICollectionViewDelegate {
         
+        var parent: ImageCollectionView
+        
+        init(_ parent: ImageCollectionView) {
+            self.parent = parent
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            
+        }
+    }
+}
+
+extension ImageCollectionView {
+    func createCollectionView(context: Context) -> UICollectionView {
+        let layout = UICollectionViewFlowLayout.customLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.reuseIdentifier)
+        collectionView.dataSource = context.coordinator
+        collectionView.delegate = context.coordinator
+        return collectionView
+    }
+}
+
+extension UICollectionViewFlowLayout {
+    static func customLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 144, height: 180)
+        layout.minimumLineSpacing = 12
+        layout.minimumInteritemSpacing = 12
+        return layout
     }
 }
